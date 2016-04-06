b4c2ff93b3efe0a20a5e8b07423010f39d6e4394
Michael Ornellas <mornellas@anyperk.com>
2016-04-06 16:20:08 -0700

    remove unnecessary boilerplate files, modify routes

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
-import counter from './modules/counter'
+import userTester from './modules/userTester'
 
 export default combineReducers({
-  counter,
+  userTester,
   router
 })
